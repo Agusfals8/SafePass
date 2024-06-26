dd23ae3c5c514ca1400831f9c7d9a2e356628c92
Agusfals8 <87876397+Agusfals8@users.noreply.github.com>
2024-06-27 02:38:46 +0300

    cleaning

--- extension/popup.js
+++ extension/popup.js
@@ -3,26 +3,33 @@ const API_URL = process.env.SAFE_PASS_API_URL;
 async function fetchPasswords() {
   try {
     const response = await fetch(`${API_URL}/passwords`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
     });
-    if (!response.ok) throw new Error("Failed to fetch passwords.");
+    
+    if (!response.ok) {
+      throw new Error('Failed to fetch passwords.');
+    }
+    
     const data = await response.json();
     displayPasswords(data);
   } catch (error) {
-    console.error("Error fetching passwords: ", error);
+    console.error('Error fetching passwords: ', error);
   }
 }
 
 function displayPasswords(passwords) {
-  const passwordsList = document.getElementById("passwords-list");
-  passwordsList.innerHTML = "";
+  const passwordsList = document.getElementById('passwords-list');
+  passwordsList.innerHTML = '';
+  
   passwords.forEach(({ id, name }) => {
-    const passwordElement = document.createElement("li");
+    const passwordElement = document.createElement('li');
     passwordElement.textContent = `${name}`;
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
+    
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
     deleteBtn.onclick = () => deletePassword(id);
+    
     passwordElement.appendChild(deleteBtn);
     passwordsList.appendChild(passwordElement);
   });
@@ -31,36 +38,46 @@ function displayPasswords(passwords) {
 async function addPassword(name, password) {
   try {
     const response = await fetch(`${API_URL}/passwords`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name, password }),
     });
-    if (!response.ok) throw new Error("Failed to add password.");
+    
+    if (!response.ok) {
+      throw new Error('Failed to add password.');
+    }
+    
     fetchPasswords();
   } catch (error) {
-    console.error("Error adding a new password: ", error);
+    console.error('Error adding a new password: ', error);
   }
 }
 
 async function deletePassword(passwordId) {
   try {
     const response = await fetch(`${API_URL}/passwords/${passwordId}`, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
     });
-    if (!response.ok) throw new Error("Failed to delete password.");
+    
+    if (!response.ok) {
+      throw new Error('Failed to delete password.');
+    }
+    
     fetchPasswords();
   } catch (error) {
-    console.error("Error deleting password: ", error);
+    console.error('Error deleting password: ', error);
   }
 }
 
 function setupForm() {
-  const addPasswordForm = document.getElementById("add-password-form");
+  const addPasswordForm = document.getElementById('add-password-form');
   addPasswordForm.onsubmit = async (e) => {
     e.preventDefault();
-    const name = document.getElementById("password-name").value;
-    const password = document.getElementById("password-value").value;
+    
+    const name = document.getElementById('password-name').value;
+    const password = document.getElementById('password-value').value;
+    
     await addPassword(name, password);
     addPasswordForm.reset();
   };
@@ -68,6 +85,7 @@ function setupForm() {
 
 function init() {
   fetchPasswords();
+  setupForm();
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener('DOMContentLoaded', init);
